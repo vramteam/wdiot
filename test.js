563230e203fcf3ffe1dcd69886fa10c34fe46e57
vramteam <49875973+vramteam@users.noreply.github.com>
2019-04-22 18:21:40 +0300

    Первый комит

---
+++ test.js
@@ -0,0 +1,17 @@
+const { remote } = require('webdriverio');
+
+(async () => {
+    const browser = await remote({
+        logLevel: 'trace',
+        capabilities: {
+            browserName: 'chrome'
+        }
+    })
+
+    await browser.url('https://webdriver.io');
+
+    const title = await browser.getTitle();
+    console.log('Title was: ' + title);
+
+    await browser.deleteSession();
+})().catch((e) => console.error(e));
